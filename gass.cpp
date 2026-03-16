269e49f20430d2af3394b26e3a1860d1d1c64cf4
himanshujalandra131-ai <himanshujalandra@gmail.com>
2026-03-16 15:07:41 +0530

    2nd  commit

---
+++ gass.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int startStation(vector<int> gas,vector<int> cost){
+    int total=0;
+    int bachhihuigas=0;
+    int ans=0;
+    for(int i=0;i<gas.size();i++){
+        total+=gas
+    }
+}
